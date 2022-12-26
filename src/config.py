5f176daf910a0aa51674047f35cabc82c2100d65
Stephano <stephano21chang@gmail.com>
2022-12-25 22:48:59 -0500

    remove files

--- src/config.py
+++ src/config.py
@@ -1,13 +1,13 @@
 class Config:
     SECRET_KEY = 'mysecretkey'
-
     
 class DevelopmentConfig(Config):
-    DEBUG = True
+    DEBUG = False
     MYSQL_HOST='localhost'
     MYSQL_USER='root'
     MYSQL_PASSWORD=''
     MYSQL_DB='camaronesa_store'
+    RUN = 3000
 
 config={
     'development': DevelopmentConfig
